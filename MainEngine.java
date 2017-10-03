0a03d53a8ea693174d5a6afb77e3e1bfb0fe1959
Yoki <camillemagic@gmail.com>
2017-10-03 01:52:25 -0400

    Update MainEngine.java

--- MainEngine.java
+++ MainEngine.java
@@ -38,7 +38,7 @@ public class MainEngine{
   
   public static void main(String[] args) throws Exception {
 	  
-    //No.1 ---------------------------------parse argument
+    //No.1 ---------------------------------parse argument----------------------------------
     if(args.length != 4){
       System.err.println("Usage: MainEngine <EngineKey> <ApiKey> <query> <precision>");
       System.exit(1);   
@@ -56,16 +56,13 @@ public class MainEngine{
       System.err.println("Please enter a valid precision number");
       System.exit(1);
     } 
-    //--------------------------------------------------------
     
-    //No.2 ----------------------------------Search && Parse
-    
-    //---------------------------------------------------
-    
+    //No.2 ----------------------------------Main: Search && Parse && Expand-----------------------------
+
     List<List<String>> rawResult = Search(engineKey, apiKey, top, query);  //return rawResult
     double realPrecision=Feedback(rawResult); //return double real prescision by user;
       if(checkPrecision(realPrecision)){
-        System.out.println("----------------------reached ideal precision----------------------");
+        System.out.println("Already reached the ideal precision!");
         System.exit(1);
       }
       else{
@@ -77,24 +74,20 @@ public class MainEngine{
         rawResult=Search(engineKey, apiKey, top, newquery);
         realPrecision = Feedback(rawResult);
         if(checkPrecision(realPrecision)){
-        System.out.println("reached ideal precision");
+        System.out.println("After Expansion, we reached the ideal precision");
         System.exit(1);
-        }
-       
-        
-        
-}
- 
-
+        	}      
+      }
   }
+  //------------------------------------Supporting Method below -------------------------------------------
   
-  // return the search query result (already finished parsing)
+  // return the raw search query result (already finished parsing)
   public static List<List<String>> Search(String engineKey, String apiKey, int top, String query) throws IOException{
  
 	List<List<String>> rawResult =new ArrayList<List<String>>();
   	HttpTransport httpTransport = new NetHttpTransport();
       JsonFactory jsonFactory = new JacksonFactory();
-      Customsearch customsearch = new Customsearch(httpTransport, jsonFactory,null);
+      Customsearch customsearch = new Customsearch.Builder(httpTransport, jsonFactory, null).setApplicationName("E6111 Search Engine").build();
       List<Result> resultList = null;
       Customsearch.Cse.List list = customsearch.cse().list(query);
       list.setKey(apiKey);
@@ -108,15 +101,13 @@ public class MainEngine{
                   rst.add(result.getFormattedUrl());
                   rst.add(result.getHtmlSnippet());
                   rawResult.add(rst);
-             
-                  //System.out.println(result.getHtmlSnippet());
-                 // System.out.println("----------------------------------------");
+
               }
        }
           return rawResult;
       }
   
-  
+
   
   // return the check precision result
   public static boolean checkPrecision(double realPrecision){
@@ -124,7 +115,7 @@ public class MainEngine{
     else return false;
   }
   
-  //collect user feedback and get the real precision 
+  //collect user feedback and return back the real precision 
   public static double Feedback(List<List<String>> rawResult){
 	  int count = 0;
 	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -165,98 +156,97 @@ public class MainEngine{
   
   
   public static String expand(String querry,List<List<String>> rawResult) throws Exception{
+	  //get relevant result
 	  List<String[]> mergingtitle = new ArrayList<String[]>();
 	  List<String[]> mergingcontent = new ArrayList<String[]>();
-
-	  //---------------- relevant word----------------
-	  // retrieve the passed title and description
 	  for(int i = 0; i< rawResult.size(); i++){
 		  if(rawResult.get(i).get(3) == "True"){
-			  String[] title = rawResult.get(i).get(0).split(" ");
+			  String rawTitle = rawResult.get(i).get(0).replaceAll("[^A-Za-z0-9]", " ").toLowerCase();
+			  String[] title = rawTitle.split(" ");
 			  mergingtitle.add(title);
-			  String[] content = rawResult.get(i).get(2).split(" ");
+			  String rawContent = rawResult.get(i).get(2).replaceAll("[^A-Za-z0-9]", " ").toLowerCase();
+			  String[] content = rawContent.split(" ");
 			  mergingcontent.add(content);
 		  }
 	  }
-	  HashSet<String> h = transfer();
 	  
-	  Double entryInverse = (double) (1/rawResult.size());
-	  HashMap<String, Double> titleNew = new HashMap<String, Double>();
-		for (String[] t : mergingtitle) {
-				for(String s: t){
-					if (h.contains(s))
+	  //stop word
+	  HashSet<String> stopwSet = transfer();
+	  
+	  //calculate df of all words
+	 // Double entryInverse = (double) (1/rawResult.size());
+	  HashMap<String, Double> weightedTitle = new HashMap<String, Double>();
+	  for (String[] title : mergingtitle) {
+		  for(String s: title){
+			  if (stopwSet.contains(s)) continue;
+			  else if (weightedTitle.containsKey(s)) {
+				  double freq = weightedTitle.get(s) +7;
+				  weightedTitle.put(s, freq);
+			  }
+			  else{
+				  weightedTitle.put(s,7.0);
+			  }		
+		  }
+	  }
+	  
+	  
+		 for (String[] content : mergingcontent) {
+				for(String s: content){
+					if (stopwSet.contains(s))
 						continue;
-					else if (titleNew.containsKey(s)) {
-						double freq = titleNew.get(s) +entryInverse;
-						titleNew.put(s, freq);
+					else if (weightedTitle.containsKey(s)) {
+						double freq = weightedTitle.get(s) +3;
+						weightedTitle.put(s, freq);
 					}
 					else{
-						titleNew.put(s,entryInverse);
-					}		
-				}
-		}
-		 //HashMap<String, Integer> contentNew = new HashMap<String, Integer>();
-		
-		
-		 for (String[] t : mergingcontent) {
-				for(String s: t){
-					if (h.contains(s))
-						continue;
-					else if (titleNew.containsKey(s)) {
-						double freq = titleNew.get(s) +entryInverse;
-						titleNew.put(s, freq);
-					}
-					else{
-						titleNew.put(s,entryInverse);
+						weightedTitle.put(s,3.0);
 					}	
 				}
 		 }
+		 
+		 // Sorting based on the relevant score
 		 String[] queryModify = query.split(" ");
-	        for(String s: queryModify){
-				if (h.contains(s))
-					continue;
-				else if (titleNew.containsKey(s)) {
-					double freq = titleNew.get(s) +1;
-					titleNew.put(s, freq);
-				}
-				else{
-					titleNew.put(s,1.0);
-				}	
-	        }
-		 Set<Entry<String, Double>> entries = titleNew.entrySet();
+		 Set<Entry<String, Double>> entries = weightedTitle.entrySet();
 		 Comparator<Entry<String, Double>> valueComparator = new Comparator<Entry<String, Double>>() {
-
-		@Override
-		public int compare(Entry<String, Double> o1, Entry<String, Double> o2) {
-			Double v1 = o1.getValue(); 
-			Double v2 = o2.getValue();
-			return v1.compareTo(v2);
-		} 
+			 		@Override
+			 		public int compare(Entry<String, Double> o1, Entry<String, Double> o2) {
+			 				Double v1 = o1.getValue(); 
+			 				Double v2 = o2.getValue();
+			 				return v1.compareTo(v2);
+			 		} 
 	   };
+	   
+	   // Retrieve the new query based on the sorting result. 
         List<Entry<String, Double>> listOfEntries = new ArrayList<Entry<String, Double>>(entries);
         Collections.sort(listOfEntries, valueComparator);
-        List<String> qReset = new ArrayList<String>();
-        int qSize=5;
-        if(query.length()>5){
-        	qSize=10;
+        List<String> queryReset = new ArrayList<String>();
+        
+       for(String s: queryModify){
+    	   queryReset.add(s.toLowerCase());
+       }
+       
+       // add new word in the new query
+        for(int i=listOfEntries.size()-1; queryReset.size()<=queryModify.length+2;i--){
+        	if(queryReset.contains(listOfEntries.get(i).getKey())) continue;
+        	queryReset.add(listOfEntries.get(i).getKey());
+
         }
-        for(int i=listOfEntries.size()-1; i>=listOfEntries.size()-qSize;i--){
-        	qReset.add(listOfEntries.get(i).getKey());
-        }
+        
         StringBuilder sb = new StringBuilder();
-        for(String s: qReset){
+        for(String s: queryReset){
         	sb.append(s);
         	sb.append(" ");
         }
         
-      // refresh the rawresult
+      // refresh the raw result list
       rawResult.clear();
       System.out.println(sb.toString());
+      
 	  return sb.toString();
 	  
   }
   
-  
+  // Put a list of common words that needs to be eliminate in the hashset. 
   public static HashSet<String> transfer() throws Exception{
 	  Scanner s = new Scanner(new File("src/stop.txt"));
 		 List<String> l = new ArrayList<String>();
@@ -269,12 +259,10 @@ public class MainEngine{
 		 for(int i = 0;i<l.size();i++){
 			 h.add(l.get(i));
 		 }
-		 // punctuation
+		 
 		 return h;
 	  
   }
-  
-
-  
+   
   
 }
